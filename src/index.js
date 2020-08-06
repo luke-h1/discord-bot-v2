443bfbd6b51de4850a06acfac26b2d44e7e9435d
luke-h1 <luke.howsam@yahoo.com>
2020-08-06 14:48:10 +0100

    revert

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 const Discord = require('discord.js');
 require('dotenv').config();
-const client = new Discord.Client(); 
-const commandHandler = require('./commands'); 
+
+const client = new Discord.Client();
+
 const prefix = '!';
 
 client.on('message', function (message) {
     if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return; 
 
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+    const commandBody = message.content.slice(prefix.length);  
+    const args = commandBody.split(' ');  
+    const command = args.shift().toLowerCase();  
 
-client.on('guildMemberAdd', (member) => {
-    const channel = member.guild.channels.cache.find(
-        (ch) => ch.name === 'member-log'
-    );
-    if (!channel) return;
-    channel.send('welcome to the server, ${member}');
+    if (command === 'ping'){
+        const timeTaken = Date.now() 
+        message.reply(`Hello, This message had a latency of ${timeTaken}ms.`)
+    }
+
+    else if (command === 'sum'){
+        const numArgs = args.map(x => parseFloat(x)); 
+        const sum = numArgs.reduce((counter, x) => counter += x);  
+        message.reply(`The sum of all the args you provide is ${num}!`)
+    } 
+
+    else if (command === 'hello'){
+        message.reply(`Beep boop I am a 🤖`)
+    }
 });
 
-client.on('message', commandHandler)
-
-client.login(process.env.BOT_TOKEN)  
-
-
-
-}); 
+client.login(process.env.BOT_TOKEN)
